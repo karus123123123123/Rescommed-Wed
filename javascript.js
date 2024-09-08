9736be47b7db6750c4a8baae5dab1c80c6b29d4d
Nguyễn Duy Tin <nduytin13112005@gmail.com>
2024-09-08 14:33:32 +0700

    update

---
+++ javascript.js
@@ -0,0 +1,33 @@
+//slider2 for other section
+document.addEventListener("DOMContentLoaded", () => {
+    // Đảm bảo biến được khởi tạo sau khi DOM đã tải
+    let currentSlidev1 = 0;
+
+    function showSlidev1(index) {
+        const slideses = document.querySelector('.slidered-items'); 
+        const totalSlide = document.querySelectorAll('.slidered-items .item').length;
+        console.log(totalSlide);
+        if (index >= 2) {
+            currentSlidev1 = 0;
+        } else if (index < 0) {
+            currentSlidev1 = 1;
+        } else {
+            currentSlidev1 = index;
+        }
+        console.log(currentSlidev1);
+        slideses.style.transform = `translateX(-${currentSlidev1 * 100}%)`; 
+    }
+
+    function nextSlidev1() {
+        showSlidev1(currentSlidev1 + 1);
+    }
+
+    function prevSlidev1() {
+        showSlidev1(currentSlidev1 - 1);
+    }
+
+    // Gán sự kiện cho các nút
+    document.getElementById("nextButton").addEventListener("click", nextSlidev1);
+    document.getElementById("prevButton").addEventListener("click", prevSlidev1);
+});
+
---
+++ main.js
@@ -0,0 +1,32 @@
+const sale = document.getElementById("image-sale");
+sale.addEventListener("click", () => {
+        window.location.href = "https://karus123123123123.github.io/Page/";
+    })
+    // const bestcombo = document.getElementById("image-combo1");
+    // bestcombo.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/Combo-foods/";
+    // })
+    // const newmenu = document.getElementById("image-combo2");
+    // newmenu.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/new-menu/";
+    // })
+    // const bestmenu = document.getElementById("image-combo3");
+    // bestmenu.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/new-menu/";
+    // })
+    // const cake = document.getElementById("image-combo4");
+    // cake.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/Cake/";
+    // })
+    // const combo4 = document.getElementById("image-combo5");
+    // combo4.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/Combo-spiece/";
+    // })
+    // const combo5 = document.getElementById("image-combo6");
+    // combo5.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/combo-delicious/";
+    // })
+    // const combo6 = document.getElementById("image-combo7");
+    // combo6.addEventListener("click", () => {
+    //     window.location.href = "https://haduckientechnology17092005.github.io/combo-special/";
+    // })
